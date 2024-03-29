16706fcb078b49a458caf5b8c5a20b408b61f3a4
D4nk <edowney29@gmail.com>
2024-03-29 00:11:20 -0400

    clean up project

---
+++ src/sign-jwt.js
@@ -0,0 +1,21 @@
+import { config } from 'dotenv';
+import { sign } from 'hono/jwt';
+
+config();
+
+const userId = 217775277349011456;
+const guildId = 1062603620887044166;
+const username = 'datboi1337';
+const permissions = 8;
+const avatar = '';
+
+sign({ userId, guildId, username, permissions, avatar }, process.env.AUTH_SECRET)
+  .then((token) => {
+    const url =
+      'https://raritynfts.xyz/roles/#token_type=Bearer&access_token=a0t5bLMMkCu0TWkj3hPLwM8qH0Rcjt&expires_in=604800&scope=applications.commands+identify&state=' +
+      token;
+    console.log(url);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
